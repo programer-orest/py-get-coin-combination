c40225c32020e500df1274ce0aeef55053f24a45
Orest Yakymiv <akimivorest@gmail.com>
2025-04-02 20:04:50 +0300

    applied the changes(flake8)

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import get_coin_combination
 
+
 class TestGetCoinCombination:
     @pytest.mark.parametrize(
         "cent_element, expected_list_of_cents",
@@ -8,25 +9,29 @@ class TestGetCoinCombination:
             pytest.param(
                 1,
                 [1, 0, 0, 0],  # corrected this line
-                id="test should return first element of list 1 because 1penny = 1cent",
+                id="""test should return first element
+                of list 1 because 1penny = 1cent""",
             ),
             pytest.param(
                 6,
                 [1, 1, 0, 0],
-                id="test should return first and second element of list 1 because 1penny = 1cent and 1nikel = 5 cent",
+                id="""test should return first and second element
+                of list 1 because 1penny = 1cent and 1nikel = 5 cent""",
             ),
             pytest.param(
                 17,
                 [2, 1, 1, 0],
-                id="test should return first element 2 and second third elements 1 because 1penny = 1cent and 1nikel = 5 cent and 1dime = 10cent",
+                id="""test should return first element 2 and
+                second third elements 1 because 1penny = 1cent
+                and 1nikel = 5 cent and 1dime = 10cent""",
             ),
             pytest.param(
                 50,
                 [0, 0, 0, 2],
-                id="test should return fourth element 2 because 1quarter = 25cent",
+                id="""test should return fourth
+                element 2 because 1quarter = 25cent""",
             ),
         ]
     )
     def test_get_coin_combination(self, cent_element, expected_list_of_cents):
         assert get_coin_combination(cent_element) == expected_list_of_cents
-
