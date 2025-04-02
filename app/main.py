c46dbc3f9e2c34785e14896666a4557a05227460
Orest Yakymiv <akimivorest@gmail.com>
2025-04-02 19:54:39 +0300

    applied the changes*

--- app/main.py
+++ app/main.py
@@ -3,10 +3,7 @@ def get_coin_combination(cents: int) -> list:
     coins = [0, 0, 0, 0]
 
     for i in range(3, -1, -1):
-        if cents < 0:
-            return [0, 0, 0, 0]
-        else:
-            coins[i] = cents // values[i]
-            cents -= coins[i] * values[i]
+        coins[i] = cents // values[i]
+        cents -= coins[i] * values[i]
 
     return coins
